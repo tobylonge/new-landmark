b35eaa88ca6bf396e86a877003ffdb5cad8cf0ef
Oluwatobi <tobylonge@gmail.com>
2020-10-26 23:12:17 +0100

    added business section

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("I got here:::")}),getDetails=a=>{const b=document.querySelector(".more-info");console.log("Lets get details ",a.getAttribute("data-slide"),a.dataset.slide),document.querySelectorAll(".service-card").forEach(a=>{a.classList.remove("active")}),a.classList.add("active"),b.innerHTML=`<p>${a.dataset.slide}</p>`};
+let swiper=new Swiper(".carousel",{centeredSlides:!0,spaceBetween:30,effect:"fade",fadeEffect:{crossFade:!0},speed:1e3,autoplay:{delay:5e3,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0}}),swiperSlider=new Swiper(".sliders",{slidesPerView:1,spaceBetween:10,preventClicksPropagation:!0,preloadImages:!1,lazy:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{"@0.00":{slidesPerView:1,slidesPerGroup:1,spaceBetween:15},"@0.75":{slidesPerView:2,slidesPerGroup:2,spaceBetween:15},"@1.00":{slidesPerView:3,slidesPerGroup:3,spaceBetween:15},"@1.50":{slidesPerView:5,slidesPerGroup:5,spaceBetween:15}}});const businessInfo=[{id:1,bImg:"img/service/bg-1.png",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:2,bImg:"img/service/bg-2.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:3,bImg:"img/service/bg-3.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:4,bImg:"img/service/bg-4.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:5,bImg:"img/service/bg-5.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:6,bImg:"img/service/bg-6.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:7,bImg:"img/service/bg-7.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:8,bImg:"img/service/bg-8.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:9,bImg:"img/service/bg-9.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{id:10,bImg:"img/service/bg-10.jpg",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}];document.addEventListener("DOMContentLoaded",()=>{console.log("I got here:::"),fetchInfo(1)}),getDetails=a=>{swiperSlider.detachEvents(),console.log("Lets get details ",a.getAttribute("data-slide"),a.dataset.slide),document.querySelectorAll(".service-card").forEach(a=>{a.classList.remove("active")}),a.classList.add("active");const b=a.getAttribute("data-slide");fetchInfo(b)},fetchInfo=a=>{const b=businessInfo.filter(b=>b.id===+a);if(console.log("id ",a,"info ",b),b){const a=document.querySelector(".more-info");a.innerHTML=`<div class="details">
+  <div class="swiper-wrapper">
+      <div class="swiper-slide">
+      <img src=${b[0].bImg} alt="" class="swiper-lazy">
+      <div class="swiper-lazy-preloader"></div>
+      </div>
+      <div class="background-overlay">
+        <div class="container mx-auto content">
+          <div class="w-2/5 py-16">
+            <p class="text-white content-text">${b[0].text}</p>
+            <ul class="text-white">
+              <li>
+                <span><i class="fa fa-phone-square mr-4 transform rotate-90 text-primary"></i>08022222232</span>
+              </li>
+              <li>
+                <span><i class="fa fa-envelope-square mr-4 text-primary"></i>test@test.org</span>
+              </li>
+              <li>
+                <span><i class="fa fa-globe mr-4 text-primary"></i>www.test.org</span>
+              </li>
+              <li>
+                <span><i class="fa fa-map-pin mr-4 text-primary"></i>Landmark retail boulevard</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </div>
+     
+  </div>
+  
+</div>`;new Swiper(".details",{preloadImages:!1,lazy:!0,effect:"fade",speed:1e3,fadeEffect:{crossFade:!0}})}};
--- js/main.js
+++ js/main.js
@@ -1,10 +1,133 @@
 // put your js here
+
+// Initialize Swiper
+let swiper = new Swiper(".carousel", {
+  centeredSlides: true,
+  spaceBetween: 30,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let swiperSlider = new Swiper(".sliders", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  preventClicksPropagation: true,
+  preloadImages: false,
+  lazy: true,
+  // init: false,
+  // mousewheel: true,
+  // keyboard: true,
+  // loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 15,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 15,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 15,
+    },
+    "@1.50": {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+      spaceBetween: 15,
+    },
+  },
+});
+
+const businessInfo = [
+  {
+    id: 1,
+    bImg: "img/service/bg-1.png",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    bImg: "img/service/bg-2.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    bImg: "img/service/bg-3.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 4,
+    bImg: "img/service/bg-4.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 5,
+    bImg: "img/service/bg-5.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 6,
+    bImg: "img/service/bg-6.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 7,
+    bImg: "img/service/bg-7.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 8,
+    bImg: "img/service/bg-8.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 9,
+    bImg: "img/service/bg-9.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 10,
+    bImg: "img/service/bg-10.jpg",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("I got here:::");
+  fetchInfo(1);
 });
 
 getDetails = (e) => {
-  const infoDiv = document.querySelector(".more-info");
+  swiperSlider.detachEvents();
+
   console.log(
     "Lets get details ",
     e.getAttribute("data-slide"),
@@ -14,5 +137,54 @@ getDetails = (e) => {
     el.classList.remove("active");
   });
   e.classList.add("active");
-  infoDiv.innerHTML = `<p>${e.dataset.slide}</p>`;
+  const id = e.getAttribute("data-slide");
+  fetchInfo(id);
+};
+
+fetchInfo = (id) => {
+  const info = businessInfo.filter((el) => el.id === Number(id));
+  console.log("id ", id, "info ", info);
+  if (info) {
+    const infoDiv = document.querySelector(".more-info");
+    infoDiv.innerHTML = `<div class="details">
+  <div class="swiper-wrapper">
+      <div class="swiper-slide">
+      <img src=${info[0].bImg} alt="" class="swiper-lazy">
+      <div class="swiper-lazy-preloader"></div>
+      </div>
+      <div class="background-overlay">
+        <div class="container mx-auto content">
+          <div class="w-2/5 py-16">
+            <p class="text-white content-text">${info[0].text}</p>
+            <ul class="text-white">
+              <li>
+                <span><i class="fa fa-phone-square mr-4 transform rotate-90 text-primary"></i>08022222232</span>
+              </li>
+              <li>
+                <span><i class="fa fa-envelope-square mr-4 text-primary"></i>test@test.org</span>
+              </li>
+              <li>
+                <span><i class="fa fa-globe mr-4 text-primary"></i>www.test.org</span>
+              </li>
+              <li>
+                <span><i class="fa fa-map-pin mr-4 text-primary"></i>Landmark retail boulevard</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </div>
+     
+  </div>
+  
+</div>`;
+    var swiperDetails = new Swiper(".details", {
+      preloadImages: false,
+      lazy: true,
+      effect: "fade",
+      speed: 1000,
+      fadeEffect: {
+        crossFade: true,
+      },
+    });
+  }
 };

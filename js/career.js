eb13e5e3519d7ca430bf1c59e4c842398cb6de8e
Oluwatobi <tobylonge@gmail.com>
2020-11-10 23:13:14 +0100

    added career page

---
+++ js/career.js
@@ -0,0 +1,36 @@
+// Initialize Swiper
+let promoSwiper = new Swiper(".career-carousel", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  preventClicksPropagation: true,
+  speed: 1000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 15,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 15,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 15,
+    },
+  },
+});

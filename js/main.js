6beb9cc6e7fb918a81d5608a93a9879564fa70fb
Oluwatobi <tobylonge@gmail.com>
2020-11-26 09:49:26 +0100

    fixing bugs raised

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   let menus = document.querySelectorAll(".menu-item a");
-  console.log(menus);
   menus.forEach((el) => {
     console.log("el ", el);
     el.addEventListener("click", (e) => {
@@ -43,5 +42,37 @@ document.addEventListener("DOMContentLoaded", () => {
       //   .querySelector(".menu-item a + .dropdown-menu")
       //   .classList.add("block");
     });
+  });
+
+  const openEls = document.querySelectorAll("[data-open]");
+  const closeEls = document.querySelectorAll("[data-close]");
+  const isVisible = "is-visible";
+
+  for (const el of openEls) {
+    el.addEventListener("click", function () {
+      const modalId = this.dataset.open;
+      document.getElementById(modalId).classList.add(isVisible);
+    });
+  }
+
+  for (const el of closeEls) {
+    el.addEventListener("click", function () {
+      this.parentElement.parentElement.parentElement.classList.remove(
+        isVisible
+      );
+    });
+  }
+
+  document.addEventListener("click", (e) => {
+    if (e.target == document.querySelector(".modal.is-visible")) {
+      document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    // if we press the ESC
+    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+      document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
   });
 });
